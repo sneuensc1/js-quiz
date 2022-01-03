ed869d3f8b5addb1f915cbd34fac9e564184e64e
saraneuie <sneuensc@umich.edu>
2022-01-02 21:33:58 -0500

    adding updates to js

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,14 @@
 //variable to store count
 var remainingTime = 75;
-
 //Variable to track whether time is running or not
 var isStopped = true;
+
+//pull up the question id
+//var questionList = getElementById("questions");
+
+let currentQuestion = 0;
+
+
 
 //Select countdown container
 const countContainer = document.getElementById("countdown-number");
@@ -57,4 +63,29 @@ startButton.onclick = startTimer;
 //submit logs your initials and score in localStorage
 
 //radio buttons 
+
+//on start set the current to 0 (the first one)
+//Display the current question by adding a class
+//On an answer:
+//- verify if its right
+
+
+
+//- If it's not subtract 10 seconds from the timer 
+//- hide the current question
+//- update current to the next question
+//- show the next question if there are more questions
+//- record the score if there aren't
+
+//var currentQuestion = 0; 
+//Then when you want the li you can do questions.children[currentQuestion]
+//And you can increase/decrease currentQuestion by 1 with ++ and --
+
+const buttonContainer = document.querySelector(input[type="radio"]);
+console.log('buttonContainer', buttonContainer);
+
+buttonContainer.addEventListener('click', event => {
+  console.log(event.target.value)
+}) 
+
 
